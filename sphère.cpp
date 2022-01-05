fe711db54e8c7d044963e908e75fee2cfe800cf7
manonschieber <manon.schieber@ecl19.ec-lyon.fr>
2022-01-05 18:18:38 +0100

    prettier

--- sphère.cpp
+++ sphère.cpp
@@ -13,6 +13,7 @@
 class Vector {
 public:
     explicit Vector(double x = 0, double y = 0, double z = 0) {
+        //constructeur. Par défaut, le point est à l'origine. 
         data[0] = x;
         data[1] = y;
         data[2] = z;
@@ -35,6 +36,7 @@ public:
 };
  
 Vector operator+(const Vector& a, const Vector& b) {
+    //addition de deux vecteurs
     return Vector(a[0] + b[0], a[1] + b[1], a[2] + b[2]);
 }
 Vector operator-(const Vector& a, const Vector& b) {
@@ -56,11 +58,11 @@ Vector cross(const Vector& a, const Vector& b) {   //produit vectoriel
     return Vector(a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]);
 }
  
-class Ray {  //une origine et une direction 
-	public:
-		Ray(const Vector& origine, const Vector& direction) : C(origine), u(direction){};
-		Vector C;
-		Vector u;
+class Ray {
+public:
+	Ray(const Vector& O, const Vector& C) : origine(O), direction(C){};
+	Vector origine;
+	Vector direction;
 };
 
 class Source { 
@@ -72,67 +74,56 @@ class Source {
 
 class Sphere {  //une origine et un rayon
     public:
-        Sphere(const Vector& O, const double R) : O(O), R(R) {};
-        bool intersect(const Ray& r, Vector& P, Vector& N) const {
-            double a = 1;
-            double b = 2*dot(r.u,r.C-O);
-            double c = (r.C - O).norm2()-R*R;
-            double delta = b*b - 4*a*c;
-            double t = (-b -sqrt(delta))/(2*a);
-            P = t*r.u+r.C;
-            N = P - O;
-            return delta>=0; 
-        };
-        double distance(const Ray& r) const {
-            double minimum = 0;
-
-            double a = 1;
-            double b = 2*dot(r.u,r.C-O);
-            double c = (r.C - O).norm2()-R*R;
-            double delta = b*b - 4*a*c;
-            double x1 = (-b -sqrt(delta))/(2*a);
-            double x2 = (-b +sqrt(delta))/(2*a);
-
-            minimum = std::min(x1,x2);
-            double distance = sqrt((minimum*r.C-O).norm2());
-            return distance;
-        };
+        Sphere(const Vector& origine, const double rayon) : O(origine), R(rayon) {};
         Vector O;
         double R;
+};
 
+bool intersect(const Ray& r, Sphere& s) {
+    double a = 1;
+    double b = 2*dot(r.direction,r.origine-s.O);
+    double c = (r.origine - s.O).norm2()-s.R*s.R;
+
+    double delta = b*b - 4*a*c;
+    if (delta<0) {return false;}
+        else{
+            double x1 = (-b -sqrt(delta))/(2*a);
+            double x2 = (-b +sqrt(delta))/(2*a);
+            if (x2 >0) {return true;}
+            else { return false;};
+    };
 };
  
 int main() {
     int W = 512;
     int H = 512;
  
-    Source source(5,Vector(0,0,0));
+    //Source source(5,Vector(0,0,0));
 
-	Sphere s(Vector(0,0,10),1);
+	Sphere s(Vector(0,0,-55),20);
 	double fov=60*M_PI/180.;   //champ visuel 
 	double tanfov2 = tan(fov/2);
 
     std::vector<unsigned char> image(W * H * 3, 0);
-    for (int i = 0; i < H; i++) {
+    for (int i = 0; i < H; i++) {   // on parcourt l'image 
         for (int j = 0; j < W; j++) {
-			Vector u(j-W/2+0.5, i-H/2+0.5, -W/(2*tan(fov/2.)));   // r(C,u) c'est la vision
+            Vector C(0,0,0);  //origine du vecteur vision
+			Vector u(j-W/2+0.5, i-H/2+0.5, -W/(2*tan(fov/2.)));   // direction du vecteur vision
 			u.normalize();
-			Vector C(0,0,0);
-			Ray r(C,u);
-			if (s.intersect(r) == true){
-                Vector p = s.P(r);
-                Vector n = s.N(p);
-				image[(i * W + j) * 3 + 0] = 255;
-            	image[(i * W + j) * 3 + 1] = 255;
-            	image[(i * W + j) * 3 + 2] = 255;
+			Ray r(C,u);  //rayon de la vision
+
+			if (intersect(r,s) == true){
+				image[(i * W + j) * 3 + 0] = 255;  //coordonnée rouge
+            	image[(i * W + j) * 3 + 1] = 255;  //coordonnée verte
+            	image[(i * W + j) * 3 + 2] = 255;  //coordonnée bleue 
 			}
 			else {
 				image[(i * W + j) * 3 + 0] = 0;
             	image[(i * W + j) * 3 + 1] = 0;
             	image[(i * W + j) * 3 + 2] = 0;
-			}
+			};
         }
-    }
+    };
     stbi_write_png("sphère.png", W, H, 3, &image[0], 0);
  
     return 0;
